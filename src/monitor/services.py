6b7dce45f3619d441c8c6a27e899d011ddc0e7ae
andoh <solomonkingsman@gmail.com>
2026-05-14 23:48:18 +0000

    add user id scope to monitor service

--- src/monitor/services.py
+++ src/monitor/services.py
@@ -15,9 +15,9 @@ class MonitorService:
     def __init__(self, session: AsyncSession) -> None:
         self.session = session
 
-    async def create_monitor(self, data: MonitorCreate):
+    async def create_monitor(self, data: MonitorCreate, user_id: uuid.UUID):
         logger.info(f"creating monitor for URL: {data.url}")
-        new_monitor = Monitor(**data.model_dump())
+        new_monitor = Monitor(**data.model_dump(), owner_id=user_id)
 
         try:
             self.session.add(new_monitor)
@@ -30,21 +30,31 @@ class MonitorService:
             logger.warning(f"Duplicate monitor attempt: {data.url}")
             raise DuplicateMonitorError
 
-    async def get_all_monitors(self) -> list[Monitor]:
-        stmt = select(Monitor).order_by(desc(Monitor.created_at))
+    async def get_user_monitors(self, user_id: uuid.UUID) -> list[Monitor]:
+        stmt = (
+            select(Monitor)
+            .where(Monitor.owner_id == user_id)
+            .order_by(desc(Monitor.created_at))
+        )
         result = await self.session.execute(stmt)
         return list(result.scalars().all())
 
-    async def get_monitor_by_id(self, id: uuid.UUID) -> Monitor:
-        statement = await self.session.execute(select(Monitor).where(Monitor.id == id))
+    async def get_monitor_by_id(
+        self, monitor_id: uuid.UUID, user_id: uuid.UUID
+    ) -> Monitor:
+        statement = await self.session.execute(
+            select(Monitor).where(Monitor.id == monitor_id, Monitor.owner_id == user_id)
+        )
         monitor = statement.scalar_one_or_none()
         if monitor is None:
             logger.info("monitor not found")
             raise MonitorNotFoundError
         return monitor
 
-    async def update_monitor(self, id: uuid.UUID, data: MonitorUpdate) -> Monitor:
-        monitor = await self.get_monitor_by_id(id)
+    async def update_monitor(
+        self, monitor_id: uuid.UUID, user_id: uuid.UUID, data: MonitorUpdate
+    ) -> Monitor:
+        monitor = await self.get_monitor_by_id(monitor_id, user_id)
 
         update_data = data.model_dump(exclude_unset=True)
         for key, value in update_data.items():
@@ -54,8 +64,8 @@ class MonitorService:
         await self.session.refresh(monitor)
         return monitor
 
-    async def delete_monitor(self, monitor_id: uuid.UUID) -> None:
-        monitor = await self.get_monitor_by_id(monitor_id)
+    async def delete_monitor(self, monitor_id: uuid.UUID, user_id: uuid.UUID) -> None:
+        monitor = await self.get_monitor_by_id(monitor_id, user_id)
         await self.session.delete(monitor)
         await self.session.commit()
         return
